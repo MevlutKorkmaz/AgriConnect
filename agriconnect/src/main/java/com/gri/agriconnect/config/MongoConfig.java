0c3ac94e608b1b2e78d4edf93a92d047982322e8
MevlutKorkmaz <korkmazme@mef.edu.tr>
2024-07-04 16:58:44 +0300

    congig creation

---
+++ agriconnect/src/main/java/com/gri/agriconnect/config/MongoConfig.java
@@ -0,0 +1,11 @@
+package com.gri.agriconnect.config;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
