0ff14ec1d4eb1432c2c1d4aa7afcea7f76d6a29e
MevlutKorkmaz <korkmazme@mef.edu.tr>
2024-07-10 09:43:15 +0300

    customexception for email duplicate

---
+++ src/main/java/com/gri/agriconnect/exception/CustomDuplicateKeyException.java
@@ -0,0 +1,7 @@
+package com.gri.agriconnect.exception;
+
+public class CustomDuplicateKeyException extends RuntimeException {
+    public CustomDuplicateKeyException(String message) {
+        super(message);
+    }
+}
